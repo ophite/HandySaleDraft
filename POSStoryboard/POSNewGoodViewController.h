17c338557df6133d8d78f177aab973c23a364500
ophite <ophite@ukr.net>
2013-12-16 19:52:24 +0200

    add new files

---
+++ POSStoryboard/POSNewGoodViewController.h
@@ -0,0 +1,13 @@
+//
+//  POSNewGoodViewController.h
+//  POSStoryboard
+//
+//  Created by kobernik.u on 12/16/13.
+//  Copyright (c) 2013 kobernik.u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POSNewGoodViewController : UIViewController
+
+@end
